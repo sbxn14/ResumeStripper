514c62aaea82c72bffd6a817c944da9941c22a52
Sander Berntsen <sander1006@hotmail.nl>
2019-02-14 11:20:55 +0100

    Created PDFGenerator

--- Helpers/PDFCreator.cs
+++ Helpers/PDFCreator.cs
@@ -4,11 +4,47 @@ using System.Linq;
 using System.Web;
 using System.IO;
 using System.Security.Cryptography;
+using Codaxy.WkHtmlToPdf;
 
 namespace ResumeStripper.Helpers
 {
     public class PDFCreator
     {
-        
+        public PdfDocument getAnonymousPdf(string HtmlCode, int id, string name)
+        {
+            return GetPdfFromHtml(HtmlCode, true, id, name);
+        }
+
+        public PdfDocument getNonymousPdf(string Htmlcode, int id, string name)
+        {
+            return GetPdfFromHtml(Htmlcode, false, id, name);
+        }
+
+        private PdfDocument GetPdfFromHtml(string HtmlCode, bool anonymous, int id, string name)
+        {
+            PdfOutput output = new PdfOutput();
+            if (anonymous)
+            {
+                //Result must be anonymous
+                output = new PdfOutput
+                {
+                    OutputFilePath = "CV_" + id.ToString() + ".pdf"
+                };
+            }
+            else
+            {
+                //result must not be anonymous
+                output = new PdfOutput
+                {
+                    OutputFilePath = "CV_" + id.ToString() + "_" + name + ".pdf"
+                };
+            }
+
+            PdfDocument document = new PdfDocument();
+
+            PdfConvert.ConvertHtmlToPdf(document, output);
+
+            return document;
+        }
     }
 }
